151e73d94b2c35e1a68d92dce18869f084082558
Irim212 <piwo855@gmail.com>
2020-02-13 01:22:10 +0100

    Login with HTTP connection (reqres.in)

--- app/src/main/java/com/example/sebastian/medi/MainActivity.java
+++ app/src/main/java/com/example/sebastian/medi/MainActivity.java
@@ -7,12 +7,30 @@ import android.text.Html;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
-    TextView appName;
+    TextView appName, emailEditText, passwordEditText;
+    String url = "http://192.168.1.100:5000/api/";
 
 
     @Override
@@ -21,23 +39,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         appName = (TextView) findViewById(R.id.textView3);
         loginButton = (Button)findViewById(R.id.loginButton);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.pwEditText);
         String text = "<font color=#cc0029>Medi</font><font color=#000000>Duty</font>";
+        emailEditText.setText("doktor@mail.com");
+        passwordEditText.setText("doktor");
         appName.setText(Html.fromHtml(text));
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(1==1){
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
 
-                    Intent userMenu = new Intent(MainActivity.this, UserMenuActivity.class);
-                    startActivity(userMenu);
+                if(validateLogin(email, password)){
+                    //do login
+                    OkHttpClient client = new OkHttpClient();
+                    String testUrl = "https://reqres.in/api/login";
 
-                }else{
+                    MediaType MEDIA_TYPE = MediaType.parse("application/json");
 
+                    JSONObject postdata = new JSONObject();
+                    try {
+                        postdata.put("email", "eve.holt@reqres.in");
+                        postdata.put("password", "asdddsdsdsss");
+                    } catch(JSONException e){
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    RequestBody body = RequestBody.create(postdata.toString(), MEDIA_TYPE);
+
+                    Request request = new Request.Builder()
+                            .url(testUrl)
+                            .post(body)
+                            .header("Accept", "application/json")
+                            .header("Content-Type", "application/json")
+                            .build();
+
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                            System.out.println("Failure");
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                            if(response.isSuccessful()){
+                                System.out.println("Login success code: " + response.code() );
+                                //Login Success
+                                final String myResponse = response.body().string();
+
+                                MainActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //Login Success
+                                        System.out.println(myResponse);
+//                                        Intent userMenu = new Intent(MainActivity.this, UserMenuActivity.class);
+//                                        startActivity(userMenu);
+                                    }
+                                });
+                            }else{
+                                System.out.println("Login error code: " + response.code() );
+                            }
+                        }
+                    });
                 }
+            }});
+        }
+
+        private boolean validateLogin(String email, String password){
+            if(email == null || email.trim().length() == 0){
+                Toast.makeText(this, "Email jest wymagany.", Toast.LENGTH_SHORT).show();
+                return false;
             }
-        });
+            if(password == null || password.trim().length() == 0){
+                Toast.makeText(this, "Password jest wymagane.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            return true;
+        }
+
     }
-
-
-}
