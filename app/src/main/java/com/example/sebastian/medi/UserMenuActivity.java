b8cab7a503afa7ba99f79c5d692fe49cebc4feb3
Irim212 <piwo855@gmail.com>
2020-02-17 20:08:14 +0100

    Exmaple JWT Token

--- app/src/main/java/com/example/sebastian/medi/UserMenuActivity.java
+++ app/src/main/java/com/example/sebastian/medi/UserMenuActivity.java
@@ -6,10 +6,11 @@ import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.CalendarContract;
-import android.provider.CalendarContract.Events;
-import android.support.v4.app.ActivityCompat;
+
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -17,17 +18,12 @@ import android.widget.TextView;
 
 import com.auth0.android.jwt.JWT;
 
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
-import static android.provider.CalendarContract.Events.*;
+import java.util.Calendar;
 
 public class UserMenuActivity extends AppCompatActivity {
 
     TextView helloText;
-    CalendarView calendarView;
     Button addDataBtn;
 
     @Override
@@ -36,14 +32,12 @@ public class UserMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_menu);
 
         helloText = (TextView) findViewById(R.id.helloTextView);
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
         addDataBtn = (Button) findViewById(R.id.addDataButton);
 
         String jwt = getIntent().getStringExtra("jwtToken");
         JWT jwtToken = new JWT(jwt);
 
         long currentTime = Calendar.getInstance().getTimeInMillis();
-        calendarView.setDate(currentTime);
 
 
         addDataBtn.setOnClickListener(new View.OnClickListener() {
@@ -58,39 +52,36 @@ public class UserMenuActivity extends AppCompatActivity {
     }
 
     public void addCalendarEvent(){
-        long calID = 3; // Make sure to which calender you want to add event
+
+        long calID = 3;
         long startMillis = 0;
         long endMillis = 0;
         Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2020, 2, 20, 8, 30);
+        beginTime.set(2020, 02, 20, 6, 00);
         startMillis = beginTime.getTimeInMillis();
         Calendar endTime = Calendar.getInstance();
-        endTime.set(2020, 2, 20, 16, 30);
+        endTime.set(2020, 02, 20, 14, 00);
         endMillis = endTime.getTimeInMillis();
-
 
         ContentResolver cr = getContentResolver();
         ContentValues values = new ContentValues();
-        values.put(DTSTART, startMillis);
-        values.put(DTEND, endMillis);
-        values.put(TITLE, "Hackathon");
-        values.put(DESCRIPTION, "do some code");
-        values.put(CALENDAR_ID, calID);
-        values.put(EVENT_TIMEZONE, TimeZone.getDefault().getID());
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Uri uri = cr.insert(CONTENT_URI, values);
+        values.put(CalendarContract.Events.DTSTART, startMillis);
+        values.put(CalendarContract.Events.DTEND, endMillis);
+        values.put(CalendarContract.Events.TITLE, "Jazzercise");
+        values.put(CalendarContract.Events.DESCRIPTION, "Group workout");
+        values.put(CalendarContract.Events.CALENDAR_ID, calID);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles");
+        values.put(CalendarContract.Events.ORGANIZER, "google_calendar@gmail.com");
 
-// get the event ID that is the last element in the Uri
-        long eventID = Long.parseLong(uri.getLastPathSegment());
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+            long eventID = Long.parseLong(uri.getLastPathSegment());
+            Log.i("Calendar", "Event Created, the event id is: " + eventID);
+            System.out.println("Jazzercise event added!");
+        } else {
+            System.out.println("Error event creating.");
+        }
+
     }
 
 }
